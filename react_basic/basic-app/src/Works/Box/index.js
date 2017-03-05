77b4c44f26e8404ac32898960609c96176d9b4a5
rei-m <star.oceans.rm@wj8.so-net.ne.jp>
2017-03-06 08:12:59 +0900

    add box

---
+++ react_basic/basic-app/src/Works/Box/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Box extends Component {
+  render() {
+    return (
+      <div className="work-box">
+        <img src={this.props.imageFileName} alt={this.props.imageAlt} className="work-image" />
+        <div className="work-description">
+          <div className="work-description-inner">
+            <p className="work-text">
+              {this.props.children}
+              <a href="#" className="button button-ghost">READ MORE</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Box;
--- react_basic/basic-app/src/Works/index.js
+++ react_basic/basic-app/src/Works/index.js
@@ -1,64 +1,33 @@
 import React, { Component } from 'react';
 import Section from '../Section';
+import Box from './Box';
 import './style.css';
 
 class Works extends Component {
   render() {
     return (
       <Section sectionName="works" title="WORKS">
-        <div class="works-wrapper">
-          <div class="work-box">
-            <img src="images/1.jpg" alt="いちまいめ" class="work-image" />
-            <div class="work-description">
-              <div class="work-description-inner">
-                <p class="work-text">
-                  ひとつめの制作事例が入ります。<br />
-                  簡単な説明が入ります。<br />
-                  ツール : XXX, XXX, XXX <br />
-                  <a href="#" class="button button-ghost">READ MORE</a>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="work-box">
-            <img src="images/2.jpg" alt="にまいめ" class="work-image" />
-            <div class="work-description">
-              <div class="work-description-inner">
-                <p class="work-text">
-                  ふたつめの制作事例が入ります。<br />
-                  簡単な説明が入ります。<br />
-                  ツール : XXX, XXX, XXX <br />
-                  <a href="#" class="button button-ghost">READ MORE</a>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="work-box">
-            <img src="images/3.jpg" alt="さんまいめ" class="work-image" />
-            <div class="work-description">
-              <div class="work-description-inner">
-                <p class="work-text">
-                  みっつめの制作事例が入ります。<br />
-                  簡単な説明が入ります。<br />
-                  ツール : XXX, XXX, XXX <br />
-                  <a href="#" class="button button-ghost">READ MORE</a>
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="work-box">
-            <img src="images/4.jpg" alt="よんまいめ" class="work-image" />
-            <div class="work-description">
-              <div class="work-description-inner">
-                <p class="work-text">
-                  よっつめの制作事例が入ります。<br />
-                  簡単な説明が入ります。<br />
-                  ツール : XXX, XXX, XXX <br />
-                  <a href="#" class="button button-ghost">READ MORE</a>
-                </p>
-              </div>
-            </div>
-          </div>
+        <div className="works-wrapper">
+          <Box imageFileName="/images/1.jpg" imageAlt="いちまいめ">
+            ひとつめの制作事例が入ります。<br />
+            簡単な説明が入ります。<br />
+            ツール : XXX, XXX, XXX <br />
+          </Box>
+          <Box imageFileName="/images/2.jpg" imageAlt="にまいめ">
+            ふたつめの制作事例が入ります。<br />
+            簡単な説明が入ります。<br />
+            ツール : XXX, XXX, XXX <br />
+          </Box>
+          <Box imageFileName="/images/3.jpg" imageAlt="さんまいめ">
+            みっつめの制作事例が入ります。<br />
+            簡単な説明が入ります。<br />
+            ツール : XXX, XXX, XXX <br />
+          </Box>
+          <Box imageFileName="/images/4.jpg" imageAlt="よんまいめ">
+            よっつめの制作事例が入ります。<br />
+            簡単な説明が入ります。<br />
+            ツール : XXX, XXX, XXX <br />
+          </Box>
         </div>
       </Section>
     );
